4c079d5c726db778019598dd8c1f42e68cea95b3
贺永胜 <1378431028@qq.com>
2020-06-14 23:57:04 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -53,20 +53,7 @@ export default {
   },
   methods: {
     showImg (url) {
-      let obj = {
-        url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592150754468&di=007303c2184d2fcd9743a285c03d5f78&imgtype=0&src=http%3A%2F%2Fimg001.hc360.cn%2Fm2%2FM06%2F59%2FCD%2FwKhQclQmgkaEWFaoAAAAADeUpXU923.jpg',
-        nowImgIndex: 0,
-        multiple: true,
-        imgList: [
-          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592150754468&di=007303c2184d2fcd9743a285c03d5f78&imgtype=0&src=http%3A%2F%2Fimg001.hc360.cn%2Fm2%2FM06%2F59%2FCD%2FwKhQclQmgkaEWFaoAAAAADeUpXU923.jpg',
-          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592150754464&di=1f138376111424a5c27a9b670cbfa262&imgtype=0&src=http%3A%2F%2Fimg.article.pchome.net%2F00%2F49%2F20%2F97%2Fpic_lib%2Fs960x639%2F14s960x639.jpg',
-          'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592150885064&di=3b8c79295faa6502bff2bf0a0b5feb23&imgtype=0&src=http%3A%2F%2Ft8.baidu.com%2Fit%2Fu%3D1484500186%2C1503043093%26fm%3D79%26app%3D86%26f%3DJPEG%3Fw%3D1280%26h%3D853'
-        ],
-      }
-      // obj = {
-      //   url: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592150754468&di=007303c2184d2fcd9743a285c03d5f78&imgtype=0&src=http%3A%2F%2Fimg001.hc360.cn%2Fm2%2FM06%2F59%2FCD%2FwKhQclQmgkaEWFaoAAAAADeUpXU923.jpg'
-      // }
-      this.$hevueImgPreview(obj)
+      this.$hevueImgPreview(url)
     }
   }
 }
